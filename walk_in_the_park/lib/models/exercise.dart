3c6a99e205edab9cb7893a7182a614e5531ba9d4
KhilfiNazarudin <201945D@mymail.nyp.edu.sg>
2022-01-17 20:28:30 +0800

    ok

---
+++ walk_in_the_park/lib/models/exercise.dart
@@ -0,0 +1,14 @@
+class Exercises {
+  Exercises(String name) {
+    this.name = name;
+  }
+  String name;
+
+  String getName() {
+    return this.name;
+  }
+
+  void setName(String name) {
+    this.name = name;
+  }
+}
